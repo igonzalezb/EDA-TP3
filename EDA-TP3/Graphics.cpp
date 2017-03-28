ad25f79ce568b7bdcd99ac84d34920dc3b8e34ef
lialvarez <lialvarez@itba.edu.ar>
2017-03-28 17:32:10 -0300

    imagen

--- EDA-TP3/Graphics.cpp
+++ EDA-TP3/Graphics.cpp
@@ -4,4 +4,26 @@
 Graphics::Graphics()
 {
 
+	return;
+}
+
+bool Graphics::createDisplay()
+{
+	al_get_display_mode(al_get_num_display_modes() - 1, &dispData);
+	display = al_create_display(dispData.width, dispData.height);
+
+	if (display == NULL)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+void Graphics::graphFloor()
+{
+	robotImg = al_load_bitmap("resources/r2d2.png");
+
 }
--- EDA-TP3/Graphics.h
+++ EDA-TP3/Graphics.h
@@ -1,18 +1,25 @@
 #ifndef _GRAPHICS_H
 #define _GRAPHICS_H
 
+#include "Robot.h"
+#include <allegro5\allegro.h>
 
 class Graphics
 {
 private:
-	unsigned int w;
-	unsigned int h;
 
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_DISPLAY_MODE dispData;
+	ALLEGRO_BITMAP  *robotImg = NULL;
 
 
 public:
+	
 	Graphics();
-
+	Graphics();
+	bool createDisplay();
+	void graphFloor();
+	void graphRobots();
 
 };
 
